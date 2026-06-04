61a770a41eca47791356852c5bde96b08d93e968
cheivoy <cherrycywong0907@gmail.com>
2026-06-04 10:48:14 +0800

    修正

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -579,7 +579,6 @@ export default function Kanban() {
     unitGroups[key].emps.push(e);
   });
 
-  const isDirty = dirtyForms.size > 0 || dirtyProgress;
   const filtTitle = filt.pm ? `${filt.pm} (${filt.unit})` : filt.unit ? `${filt.proj}/${filt.unit}` : filt.proj || "全部員工";
 
   const isDirty = dirtyForms.size > 0 || dirtyProgress;
